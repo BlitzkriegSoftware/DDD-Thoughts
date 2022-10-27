0c2d3197051c5d6749d266966c167366cf578f05
Stuart Williams <spookdejur@hotmail.com>
2022-10-26 17:38:13 -0700

    Initial

---
+++ DDD-thoughts-test-example/DDD-tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDD_thoughts_test_example
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
